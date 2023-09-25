73b52f0897278c2282b9a6b47f29a7ca2541e657
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-09-25 14:21:26 +0530

    server initialize

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+import connectDB from "./db/connectDB.js";
+import userRoutes from "./routes/userRoutes.js";
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
+);
